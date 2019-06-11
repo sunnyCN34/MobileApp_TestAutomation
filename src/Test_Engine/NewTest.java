b85867cc5abb8a2370ee9ee39be32cd384dc7183
s_sahu <ssahu@dcu.org>
2019-06-11 09:27:09 -0400

    add to new repo

---
+++ src/Test_Engine/NewTest.java
@@ -0,0 +1,39 @@
+package Test_Engine;
+
+import io.appium.java_client.android.AndroidDriver;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	 
+	private static AndroidDriver driver;
+
+@Test	 
+		public static void test() throws InterruptedException, MalformedURLException {
+
+			File classpathRoot = new File(System.getProperty("user.dir"));
+			File appDir = new File(classpathRoot, "/data/app/");
+			File app = new File(appDir, "base.apk");
+
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+			capabilities.setCapability("deviceName", "Sunny's Phone");
+			capabilities.setCapability("platformVersion", "8.0.0");
+			capabilities.setCapability("platformName", "Android");
+			capabilities.setCapability("app", app.getAbsolutePath());
+			capabilities.setCapability("appPackage", "com.Vertifi.Mobile.P211391825");
+			capabilities.setCapability("appActivity", "com.Vertifi.Mobile.MainActivity");
+
+			driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+			driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+			Thread.sleep(10000);
+			driver.quit();
+  }
+}
